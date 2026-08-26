276ac2ccd860fd557a6ff785a9bb30f76880cd49
naharran <nahar1995@gmail.com>
2026-08-26 18:06:49 +0300

    fix server run script

--- server/dist/server/server.js
+++ server/dist/server/server.js
@@ -1,9 +1,5 @@
 import express from "express";
-import { updateUser } from "./users-db.js";
 const app = express();
-app.put("/", (req, res) => {
-});
-updateUser(1, { name: "hello" });
 app.listen(3000, () => {
     console.log("Started listening on port 3000");
 });
